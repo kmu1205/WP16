67d9bbb7cf92509d1993da5fe75aad2a9a28c72b
juheeWebzen <juhee@webzen.com>
2016-02-02 16:08:22 +0900

    add job

---
+++ JuHeeUMGTest/Source/JuHeeUMGTest/MyClassTest.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "JuHeeUMGTest.h"
+#include "MyClassTest.h"
+
+MyClassTest::MyClassTest()
+{
+	hahahahahah = 0.9f;
+}
+
+MyClassTest::~MyClassTest()
+{
+}
