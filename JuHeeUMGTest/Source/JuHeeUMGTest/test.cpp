00afe298e33fef0cf58e12e1ab3988fc66e789e1
juheeWebzen <juhee@webzen.com>
2016-02-02 16:23:13 +0900

    today final change

---
+++ JuHeeUMGTest/Source/JuHeeUMGTest/test.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "JuHeeUMGTest.h"
+#include "test.h"
+
+Atest::Atest()
+{
+	ha = 0.9f;
+
+}
+
+
---
+++ JuHeeUMGTest/Source/JuHeeUMGTest/test.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "JuHeeUMGTestCharacter.h"
+#include "test.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JUHEEUMGTEST_API Atest : public AJuHeeUMGTestCharacter
+{
+	GENERATED_BODY()
+public:
+
+	Atest();
+
+		UPROPERTY()
+		float ha;
+	
+	
+};
